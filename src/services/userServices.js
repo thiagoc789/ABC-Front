937e1d1212c1515123107fdf58f95b1d8c3ebc18
Juan Diego Garcia <juandiego0827@gmail.com>
2022-06-26 23:47:55 -0500

    Login

--- src/services/userServices.js
+++ src/services/userServices.js
@@ -2,6 +2,16 @@ import axios from "axios";
 import ROUTES from "./apiRoutes";
 
 class userServices {
+
+  login(data){
+    return axios.post(`${ROUTES.USER}/login/`, data)
+  }
+  getUserAuthenticated(){
+    return axios.get(`${ROUTES.USER}/user-authenticated/`)
+  }
+  logout(){
+    return axios.post(`${ROUTES.USER}/logout/`)
+  }
   getUsers() {
     console.log(ROUTES.USER);
     return axios.get(`${ROUTES.USER}/`);

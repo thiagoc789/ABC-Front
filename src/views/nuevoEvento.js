a107a51a73c669686f10b007677396c780321c33
Van <45314803+vantwot@users.noreply.github.com>
2022-08-06 17:36:13 -0500

    Solucionado problema de margen

--- src/views/nuevoEvento.js
+++ src/views/nuevoEvento.js
@@ -1,131 +1,128 @@
-              import React, { useState } from 'react'
-              import Container from '@material-ui/core/Container';
-              import eventServices from 'services/eventServices';
-              import {
-                Card,
-                CardHeader,
-                CardBody,
-                CardTitle,
-                Table,
-                Row,
-                Col,
-              } from "reactstrap";
+                import React, { useState } from 'react'
+                import Container from '@material-ui/core/Container';
+                import eventServices from 'services/eventServices';
+                import {
+                  Card,
+                  CardHeader,
+                  CardBody,
+                  CardTitle,
+                  Table,
+                  Row,
+                  Col,
+                } from "reactstrap";
 
 
-              const App = () => {
+                const App = () => {
 
-                const initialTutorialState = {
-                  title: "",
-                  content: "",
-                  image: ""
-                };
+                  const initialTutorialState = {
+                    title: "",
+                    content: "",
+                    image: ""
+                  };
 
-                const [imagen, setImagen] = useState("");
-                const [url, setUrl] = useState("");
-                const [tutorial, setTutorial] = useState(initialTutorialState);
-                const [submitted, setSubmitted] = useState(false);
+                  const [imagen, setImagen] = useState("");
+                  const [url, setUrl] = useState("");
+                  const [tutorial, setTutorial] = useState(initialTutorialState);
+                  const [submitted, setSubmitted] = useState(false);
 
-                const handleInputChange = event => {
-                  const { name, value } = event.target;
-                  setTutorial({ ...tutorial, [name]: value });
-                };
-
-
-                const saveTutorial = () => {
-                  let form_data = new FormData();
-                  form_data.append('title', tutorial.title);
-                  form_data.append('content', tutorial.content);
-                  form_data.append('image', url);
-                  console.log(url)
-                  eventServices.createUser(form_data)
-                  window.location = "eventos"
-                };
+                  const handleInputChange = event => {
+                    const { name, value } = event.target;
+                    setTutorial({ ...tutorial, [name]: value });
+                  };
 
 
-                const uploadImage = () => {
-                  const data = new FormData()
-                  data.append("file", imagen)
-                  data.append("upload_preset", "wansxszh")
-                  data.append("cloud_name", "Structum")
-                  fetch("https://api.cloudinary.com/v1_1/Structum/upload", {
-                    method: "post",
-                    body: data
-                  })
-                    .then(resp => resp.json())
-                    .then(data => {
-                      setUrl(data.url)
-                      console.log(url)
+                  const saveTutorial = () => {
+                    let form_data = new FormData();
+                    form_data.append('title', tutorial.title);
+                    form_data.append('content', tutorial.content);
+                    form_data.append('image', url);
+                    console.log(url)
+                    eventServices.createUser(form_data)
+                    window.location = "eventos"
+                  };
+
+
+                  const uploadImage = () => {
+                    const data = new FormData()
+                    data.append("file", imagen)
+                    data.append("upload_preset", "wansxszh")
+                    data.append("cloud_name", "Structum")
+                    fetch("https://api.cloudinary.com/v1_1/Structum/upload", {
+                      method: "post",
+                      body: data
                     })
-                    .catch(err => console.log(err))
-                    
-                }
+                      .then(resp => resp.json())
+                      .then(data => {
+                        setUrl(data.url)
+                        console.log(url)
+                      })
+                      .catch(err => console.log(err))
+                      
+                  }
 
-                const newTutorial = () => {
-                  setTutorial(initialTutorialState);
-                  setSubmitted(false);
-                };
+                  const newTutorial = () => {
+                    setTutorial(initialTutorialState);
+                    setSubmitted(false);
+                  };
 
-                return (
-                  <Row>
-                  <Col md="12">
-                  <Container component="main" maxWidth="xs">
-                  <div className="submit-form">
-                    {submitted ? (
-                      <div>
-                        <h4>You submitted successfully!</h4>
-                        <button className="btn btn-success" onClick={newTutorial}>
-                          Add
-                        </button>
-                      </div>
-                    ) : (
-                      <div>
-                          <div className="form-group">
-                          <label htmlFor="title">Title</label>
-                          <input
-                            type="text"
-                            className="form-control"
-                            id="title"
-                            required
-                            value={tutorial.title}
-                            onChange={handleInputChange}
-                            name="title"
-                          />
+                  return (
+                    <Container  component="main" maxWidth="xs">
+                    <div className="caja"> <h1 style={{ color:"#F4F3EF" }}> /n </h1></div>
+                    <div className="submit-form">
+                      {submitted ? (
+                        <div>
+                          <h4>You submitted successfully!</h4>
+                          <button className="btn btn-success" onClick={newTutorial}>
+                            Add
+                          </button>
                         </div>
-                        <div className="form-group">
-                          <label htmlFor="content">Content</label>
-                          <input
-                            type="text"
-                            className="form-control"
-                            id="content"
-                            required
-                            value={tutorial.content}
-                            onChange={handleInputChange}
-                            name="content"
-                          />
+                      ) : (
+                        <div>
+                            <div className="form-group">
+                            <label htmlFor="title">Título</label>
+                            <input
+                              type="text"
+                              className="form-control"
+                              id="title"
+                              required
+                              value={tutorial.title}
+                              onChange={handleInputChange}
+                              name="title"
+                            />
                           </div>
+                          <div className="form-group">
+                            <label htmlFor="content">Descripción</label>
+                            <input
+                              type="text"
+                              className="form-control"
+                              id="content"
+                              required
+                              value={tutorial.content}
+                              onChange={handleInputChange}
+                              name="content"
+                            />
+                            </div>
 
-                        <div className="form-group">
-                          <label htmlFor="content">Subir imagen</label>
-                          <input
-                            type="file"
-                            className="form-control"
-                            required
-                            onChange={(e) => setImagen(e.target.files[0])}
-                          />
+                          <div className="form-group">
+                            <label htmlFor="content">Subir imagen</label>
+                            <input
+                              type="file"
+                              className="form-control"
+                              required
+                              onChange={(e) => setImagen(e.target.files[0])}
+                            />
+                          </div>
+                        <button onClick={uploadImage}>Upload</button> 
+
+                        <img src={url} />
+                        {url.length>0 && <p>{url}</p>}
+                          <button onClick={saveTutorial} className="btn btn-success">
+                            Crear nuevo evento 
+                          </button>
                         </div>
-                      <button onClick={uploadImage}>Upload</button> 
-
-                      <img src={url} />
-                      {url.length>0 && <p>{url}</p>}
-                        <button onClick={saveTutorial} className="btn btn-success">
-                          Crear nuevo evento 
-                        </button>
-                      </div>
-                    )}
-                  </div>
-                  </Container>
-                  </Col>
-                </Row>
-                )
-              }
-              export default App;
+                      )}
+                    </div>
+                    </Container>
+                  )
+                }
+                export default App;

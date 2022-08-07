81f14e39924d1275bc9469bfac118cfbb4a58ef2
Juan Diego Garcia <juandiego0827@gmail.com>
2022-08-07 08:51:50 -0500

    Fixes

---
+++ src/services/userServices.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+import ROUTES from "./apiRoutes";
+
+class userServices {
+
+  login(data){
+    return axios.post(`${ROUTES.USER}/login/`, data)
+  }
+  logout(){
+    return axios.post(`${ROUTES.USER}/logout/`)
+  }
+  getUsers() {    
+    return axios.get(`${ROUTES.USER}/`);
+  }
+  createUser(values) {
+    return axios.post(`${ROUTES.USER}/register/`, values);
+  }
+  updateUser(id, values) {
+    return axios.put(`${ROUTES.USER}/${id}`, values);
+  }
+  getSingleUser(id) {
+    return axios.get(`${ROUTES.USER}/${id}`);
+  }
+  deleteUser(id) {
+    return axios.delete(`${ROUTES.USER}/${id}`);
+  }
+}
+
+export default new userServices();
